5997905acff14f54bf4ef84fe2772c165b5ba5be
raghib <raghibshahriar6@gmail.com>
2024-09-06 15:14:17 +0600

    added story model

--- story_app/models.py
+++ story_app/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Story(MPTTModel):
+    author = models.ForeignKey(User, related_name="stories", on_delete=models.CASCADE)
+    title = models.CharField(max_length=150, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    details = models.TextField(null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['parent','title']
+
+    def __str__(self):
+        return self.title
